7e80499927a14117791a96fcad9e4d48a2e21288
unknown <danwilliams23.dw@gmail.com>
2015-03-01 19:46:28 +0000

    Initial commit

---
+++ app/src/main/java/uk/me/danwilliams/interactivestory/model/Choice.java
@@ -0,0 +1,30 @@
+package uk.me.danwilliams.interactivestory.model;
+
+/**
+ * Created by Daniel on 23/02/2015.
+ */
+public class Choice {
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage){
+        mText = text;
+        mNextPage = nextPage;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
